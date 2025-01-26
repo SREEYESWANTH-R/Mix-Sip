1184b19549abf00f104ad197a01a65a9b86f1dcf
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-01-26 18:39:29 +0530

    backend for cart-count,add-cart function

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -6,30 +6,29 @@ import Product from "../model/ProductModel.js";
 
 const router = express.Router();
 
-router.put("/:id",authMiddleware,async(req,res)=>{
+// Route to add product to cart
+router.post("/:id",authMiddleware,async(req,res)=>{
     try {
         const userId = req.user.id;
-        
         const {id} = req.params;
-    
         const user = await User.findById(userId);
         if(!user){
             return res.status(401).json("User Not Authenticated");
         }
 
         const product = await Product.findById(id);
-
+        
         if(!product){
             return res.status(404).json("Product Not found");
         }
 
-        const existingProductIndex = user.cart.find((item)=>item.productId === id);
-
-        if(existingProductIndex > 0) {
-            user.cart[existingProductIndex].productQuantity += 1;
+        const existingProductIndex = user.cart.findIndex((item) => item.productId === id);
+        
+        if(existingProductIndex >= 0) {
+            user.cart[existingProductIndex].quantity += 1;
         }else{
             user.cart.push({
-                id,
+                productId:id,
                 name: product.name,
                 price: product.price,
                 quantity: 1,
@@ -41,6 +40,27 @@ router.put("/:id",authMiddleware,async(req,res)=>{
         res.status(200).json({message:"Added to cart"});
 
     } catch (error) {
+        res.status(500).json("Server Error");
+    }
+})
+
+//Route to show the cart-count
+router.get("/cart-count",authMiddleware,async(req,res)=>{
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(404).json("User Not Found");
+        }
+
+        const totalItems = user.cart.reduce((total,item) => total+item.quantity,0);
+        res.status(200).json({
+            totalItems
+        })
+
+    } catch (error) {
+        console.log(error)
         res.status(500).json("Server Error");
     }
 })
