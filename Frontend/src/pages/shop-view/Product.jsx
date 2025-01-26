646b0c8ab0e8807687739320797d47f2ecec4cbb
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-01-26 23:16:53 +0530

    changed fetch function to redux state

--- Frontend/src/pages/shop-view/Product.jsx
+++ Frontend/src/pages/shop-view/Product.jsx
@@ -1,15 +1,28 @@
-import { useParams } from "react-router-dom"
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchInidividualProd } from "../../store/product-slice";
 
 const Product = () => {
+  const { id } = useParams();
+  const { singleProduct, loading, error } = useSelector(state => state.product);
 
+  const dispatch = useDispatch();
 
-  const {id} = useParams();
-  console.log(id)
+  useEffect(
+    () => {
+      dispatch(fetchInidividualProd(id));
+    },
+    [dispatch, id]
+  );
+
+  console.log(singleProduct);
 
   return (
-    <div>Product</div>
-  )
-}
+    <div>
+       {singleProduct ? <h1>{singleProduct.name}</h1> : <p>Product not found</p>}
+    </div>
+  );
+};
 
-export default Product
+export default Product;
