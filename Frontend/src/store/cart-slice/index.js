fd48dfff1b92a9990d6a4b35b2f2310e3935ce10
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-01-30 16:26:47 +0530

    slice for cart count added

---
+++ Frontend/src/store/cart-slice/index.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  cartCount: 0,
+  error: null
+};
+
+export const getCartCount = createAsyncThunk("cart/cart-count", async () => {
+  const response = await axios.get("http://localhost:5000/api/cart/cart-count",{
+    withCredentials:true
+  });
+  return response.data.totalItems;
+});
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(getCartCount.pending, state => {
+      state.loading = true;
+    }).addCase(getCartCount.fulfilled,(state,action)=>{
+        state.loading = false,
+        state.cartCount = action.payload
+    }).addCase(getCartCount.rejected,(state,action)=>{
+        state.loading = false,
+        state.cartCount = action.error.message
+    })
+  }
+});
+
+export default cartSlice.reducer;
