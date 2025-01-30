5d78f1b9f84a0cf664628505dc18d5f07821e76a
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-01-30 16:26:59 +0530

    added cart reducer

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './auth-slice/index';
 import productReducer from './product-slice/index';
+import cartReducer from './cart-slice/index';
 
 
 
@@ -8,6 +9,7 @@ const store = configureStore({
     reducer:{
         auth:authReducer,
         product:productReducer,
+        cart:cartReducer
     }
 })
 
