fca5f9a95d98dbdf198afb9fc23d843b54764cb1
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-01-31 20:01:53 +0530

    added footer and profile menus

--- Frontend/src/compnent/shop/Layout.jsx
+++ Frontend/src/compnent/shop/Layout.jsx
@@ -4,39 +4,24 @@ import searchIcon from "../../assets/search_icon.png";
 import cartIcon from "../../assets/cart_icon.png";
 import profileIcon from "../../assets/profile_icon.png";
 import { useEffect } from "react";
-
+import { Facebook, Instagram, Twitter } from "lucide-react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCartCount } from "../../store/cart-slice";
 
 function Layout() {
-  // const [totalCartCount, setTotalCartCount] = useState(0);
   const { cartCount } = useSelector(state => state.cart);
   const dispatch = useDispatch();
 
   useEffect(
     () => {
-      // const getCartCount = async() =>{
-      //   try {
-      //     const response = await axios.get("http://localhost:5000/api/cart/cart-count",{
-      //       withCredentials:true
-      //     })
-      //     if(response){
-      //       setTotalCartCount(response.data.totalItems);
-      //       console.log(response.data.totalItems)
-      //     }
-      //   } catch (error) {
-      //     console.log(error)
-      //   }
-      // }
-      // getCartCount();
       dispatch(getCartCount());
     },
     [dispatch, cartCount]
   );
 
   return (
-    <div className="flex flex-col gap-4 px-24 ">
-      <nav className="px-12 py-5 flex items-center justify-between">
+    <div className="flex flex-col gap-4 min-h-screen">
+      <nav className="px-12 py-5 flex items-center justify-between ">
         <img src={logo} className="w-20" alt="" />
         <ul className="hidden sm:flex gap-7 text-lg text-gray-700 font-semibold">
           <NavLink
@@ -80,13 +65,81 @@ function Layout() {
               <img className="w-6" src={cartIcon} alt="Cart" />
             </Link>
           </div>
-
-          <img className="w-6" src={profileIcon} alt="" />
+          <div className="relative group">
+            <img
+              className="w-6 cursor-pointer"
+              src={profileIcon}
+              alt="Profile"
+            />
+            <ul className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-lg p-2 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300">
+              <Link to="/profile">
+                <li className="p-2 hover:bg-gray-100 cursor-pointer">
+                  Profile
+                </li>
+              </Link>
+              <Link to="/address">
+                <li className="p-2 hover:bg-gray-100 cursor-pointer">
+                  Manage Address
+                </li>
+              </Link>
+              <Link to="/orders">
+                <li className="p-2 hover:bg-gray-100 cursor-pointer">Orders</li>
+              </Link>
+              <Link to="/logout">
+                <li className="p-2 hover:bg-gray-100 cursor-pointer">Logout</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </nav>
-      <div>
+      <div className="flex-grow">
         <Outlet />
       </div>
+
+      {/* Footer section */}
+      <footer className="bg-pink-200 text-white py-6 w-full">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-6 md:px-12">
+          {/* Logo & Name */}
+          <div className="text-center md:text-left">
+            <img src={logo} alt="Mix & Sip Logo" className="h-auto w-20" />
+            <p className="text-black mt-1">Refreshing Every Sip!</p>
+          </div>
+
+          {/* Navigation Links */}
+          <div className="flex space-x-6 my-4 md:my-0">
+            <a href="/" className="text-black hover:text-white">
+              Home
+            </a>
+            <a href="/about" className="text-black hover:text-white">
+              About
+            </a>
+            <a href="/menu" className="text-black hover:text-white">
+              Menu
+            </a>
+            <a href="/contact" className="text-black hover:text-white">
+              Contact
+            </a>
+          </div>
+
+          {/* Social Media Icons */}
+          <div className="flex space-x-4">
+            <a href="#" className="text-black hover:text-white text-xl">
+              <Facebook />
+            </a>
+            <a href="#" className="text-black hover:text-white text-xl">
+              <Instagram />
+            </a>
+            <a href="#" className="text-black hover:text-white text-xl">
+              <Twitter />
+            </a>
+          </div>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-center text-black text-sm mt-4">
+          © {new Date().getFullYear()} Mix & Sip. All rights reserved.
+        </div>
+      </footer>
     </div>
   );
 }
